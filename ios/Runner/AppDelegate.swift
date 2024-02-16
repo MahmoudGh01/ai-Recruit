7b4bfd081ce91580a58913c627b1903d1748edaf
MahmoudGh01 <mahmoud.gharbi@esprit.tn>
2024-02-16 20:41:42 +0100

    AppCenter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +10,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      AppCenter.start(withAppSecret: "fcb4bef7-3432-41f2-8df7-01330afd03ef", services:[
+        Analytics.self,
+        Crashes.self
+      ])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
