bad1a8f044380882907813106433d308ed122085
MahmoudGh01 <mahmoud.gharbi@esprit.tn>
2024-03-01 01:26:42 +0100

    Update appcenter-post-clone.sh

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -18,4 +18,13 @@ echo "Installed flutter to `pwd`/flutter"
 cd ./ios
 flutter precache --ios
 flutter pub get
-pod install 
+pod install
+flutter build ios --release
+
+post_install do |installer|
+ installer.pods_project.targets.each do |target|
+  target.build_configurations.each do |config|
+   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
+  end
+ end
+end
